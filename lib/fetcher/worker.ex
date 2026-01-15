fe905fce0832acd33b71f0f329c31a101d27832c
Justshantam <writetoshantam@gmail.com>
2026-01-15 19:21:00 +0530

    Add rate-limited fetcher worker using Finch

--- lib/fetcher/worker.ex
+++ lib/fetcher/worker.ex
@@ -5,4 +5,50 @@ defmodule ResearchScraper.Fetcher.Worker do
   This module will later be implemented as a GenServer
   or simple task-based worker.
   """
+  use GenServer
+
+  ## Public API
+
+  def start_link(_opts \\ []) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @doc """
+  Fetches the given URL if permitted by the rate limiter.
+  """
+
+  def fetch(pid, url) do
+    GenServer.call(pid, {:fetch, url}, 10_000)
+  end
+
+  ## GenServer Callbacks
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call({:fetch, url}, _from, state) do
+    case ResearchScraper.RateLimiter.request_permission() do
+      :ok ->
+        response = do_fetch(url)
+        {:reply, response, state}
+      :error ->
+        {:reply, {:error, :rate_limited}, state}
+    end
+  end
+
+  ## Internal Helpers
+
+  defp do_fetch(url) do
+    request = Finch.build(:get, url)
+
+    case Finch.request(request, ResearchScraperFinch) do
+      {:ok, %Finch.Response{status: status, body: body}} ->
+        {:ok, %{status: status, body: body}}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
 end
--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule ResearchScraper.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:finch, "~> 0.18"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
