9b2ed427f2acc069e1dfa8decbd2a8af6650b2a7
shantam-sharma <writetoshantam@gmail.com>
2026-01-16 20:27:51 +0530

    Replace deprecated simple_one_for_one with DynamicSupervisor for fetcher pool

--- lib/fetcher/fetcher.ex
+++ lib/fetcher/fetcher.ex
@@ -1,5 +1,26 @@
 defmodule ResearchScraper.Fetcher do
   @moduledoc """
-  Supervises fetcher worker processes responsible for HTTP requests.
+  Dynamically supervises fetcher worker processes.
+
+  Uses DynamicSupervisor (modern replacement for :simple_one_for_one).
   """
+  use DynamicSupervisor
+
+  alias ResearchScraper.Fetcher.Worker
+
+  def start_link(_opts \\ []) do
+    DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  @impl true
+  def init(:ok) do
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
+
+  @doc """
+  Starts a new fetcher worker under supervision.
+  """
+  def start_worker do
+    DynamicSupervisor.start_child(__MODULE__, {Worker, []})
+  end
 end
